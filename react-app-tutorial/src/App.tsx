419854ae1dfb01896667d987126933a89aea7845
Brian David <bdavid1530@gmail.com>
2023-07-08 18:33:58 +0100

    Created expense table
    Started to create the table for recording the expenses.

--- react-app-tutorial/src/App.tsx
+++ react-app-tutorial/src/App.tsx
@@ -5,11 +5,23 @@ import ListGroup from "./components/ListGroup";
 import ExpandableText from "./components/ExpandableText";
 import Form from "./components/Form";
 import ExpenseInput from "./expense-tracker/ExpenseList/ExpenseInput/ExpenseInput";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+
+const categories = ['Entertainment', 'Utilities', 'Groceries'];
 
 function App() {
 
+  const expenses = [
+    {id: 1, description: 'Product1', amount: 1, category: 'Groceries'},
+    {id: 2, description: 'Product2', amount: 1, category: 'Groceries'},
+    {id: 3, description: 'Product3', amount: 1, category: 'Groceries'},
+    {id: 4, description: 'Product4', amount: 1, category: 'Groceries'}
+  ];
+  
+
   return(
-    <ExpenseInput></ExpenseInput>
+    <ExpenseList expenses={expenses} onDelete={() => console.log('Deleted')}></ExpenseList>
+    // <ExpenseInput types={categories}></ExpenseInput>
   )
 
 }
