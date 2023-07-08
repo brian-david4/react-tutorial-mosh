3e301e2758db04353a8dc40fe08c57fec0258ef4
Brian David <bdavid1530@gmail.com>
2023-07-08 18:35:34 +0100

    Trying to create input fields

--- react-app-tutorial/src/expense-tracker/ExpenseList/ExpenseInput/ExpenseInput.tsx
+++ react-app-tutorial/src/expense-tracker/ExpenseList/ExpenseInput/ExpenseInput.tsx
@@ -1,15 +1,26 @@
 import React from "react";
-import styles from './ExpenseInput.module.css';
+import styles from "./ExpenseInput.module.css";
+import { useState } from "react";
 
-const ExpenseInput = () => {
+interface InputProps {
+  types: string[];
+}
+
+const ExpenseInput = ({ types }: InputProps) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <form>
       <div className="mb-3">
-        <label htmlFor="description" className="form-label">Description</label>
+        <label htmlFor="description" className="form-label">
+          Description
+        </label>
         <input id="description" type="text" className="form-control" />
       </div>
       <div className="mb-3">
-        <label htmlFor="Amount" className="form-label">Amount</label>
+        <label htmlFor="Amount" className="form-label">
+          Amount
+        </label>
         <input id="Amount" type="text" className="form-control" />
       </div>
       <button className={styles.button}>Submit</button>
