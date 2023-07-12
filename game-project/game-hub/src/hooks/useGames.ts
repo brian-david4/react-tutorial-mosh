d26f096f56d36f7068130879025b51017754c465
Brian David <bdavid1530@gmail.com>
2023-07-12 16:40:55 +0100

    Construct GameCards

--- game-project/game-hub/src/hooks/useGames.ts
+++ game-project/game-hub/src/hooks/useGames.ts
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface GamesRes {
@@ -21,9 +23,12 @@ const useGames = () => {
     apiClient
       .get<GamesRes>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
+      .catch((err) => {
+        if(err instanceof CanceledError) return;
+        setError(err.message)
+       });
 
-    return () => controller.abort;
+    return () => controller.abort();
   }, []);
 
   return { games, error};
