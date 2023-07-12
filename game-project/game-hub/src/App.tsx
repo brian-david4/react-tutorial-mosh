7f752ae67f932c656fad1acef0168bea40841e5b
Brian David <bdavid1530@gmail.com>
2023-07-12 11:03:15 +0100

    Implement dark mode

--- game-project/game-hub/src/App.tsx
+++ game-project/game-hub/src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         }}
       >
         <GridItem area="nav">
-          Nav
+          <NavBar />
         </GridItem>
         <Show above="lg">
           <GridItem area="aside" bg="gold">
