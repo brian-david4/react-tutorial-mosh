0010498484b9ac81444bfa539fe6c1749dac8e52
Brian David <bdavid1530@gmail.com>
2023-07-17 11:01:37 +0100

    Genre Clean up

--- game-project/game-hub/src/components/GenreList.tsx
+++ game-project/game-hub/src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -23,15 +24,19 @@ const GenreList = ({ onSelectGenre, selectedGenre }: GenreListProps) => {
   return (
     <>
       <List>
+        <Heading fontSize={'2xl'} marginBottom={3}>Genre</Heading>
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY={"5px"}>
             <HStack>
               <Image
+                objectFit={'cover'}
                 boxSize={"32px"}
                 borderRadius={8}
                 src={getCroppedImageUrl(genre.image_background)}
               />
               <Button
+                textAlign={'left'}
+                whiteSpace={'nowrap'}
                 fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
                 variant={"link"}
